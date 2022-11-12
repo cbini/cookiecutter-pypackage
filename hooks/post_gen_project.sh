c6ddb49c95d3b92258ed37c477b35cc5ba35020c
Charlie <5003326+cbini@users.noreply.github.com>
2022-11-12 07:52:03 -0600

    asdf

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,8 +10,8 @@ gh repo create --private --source=. --remote=upstream
 function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/\$@; }
 gi linux,macos,windows,python >.gitignore
 
-# init trunk, accept all defaults
-trunk init -y
+# # init trunk, accept all defaults
+# trunk init -y
 
-# init dynaconf
-pdm run dynaconf init -f toml --path=.dynaconf/
+# # init dynaconf
+# pdm run dynaconf init -f toml
