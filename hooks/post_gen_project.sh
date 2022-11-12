6d4c693f4448a81061159fce39bcdce6f58ef634
Charlie <5003326+cbini@users.noreply.github.com>
2022-11-12 08:03:51 -0600

    commit

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,15 +4,19 @@
 pdm add -d mkdocs towncrier pytest tox dynaconf
 
 # init git repo and add to GitHub
-git init -b main
+git init -b=main -q
 gh repo create --private --source=. --remote=upstream
 
 # create .gitignore
 function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/\$@; }
 gi linux,macos,windows,python >.gitignore
 
-# init trunk, accept all defaults
-trunk init -y
-
 # init dynaconf
-pdm run dynaconf init -f toml
+pdm run dynaconf init -f=toml
+
+# commit new files
+git add .
+git commit -m "Initial commit"
+
+# init trunk, accept all defaults
+trunk init -y --no-progress
