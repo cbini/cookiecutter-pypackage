65a3776a824d90c51ce0494c5af0a2bf07e19a3e
Charlie <5003326+cbini@users.noreply.github.com>
2022-11-12 07:47:26 -0600

    fix hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,15 +3,15 @@
 # init pyproject and install dev dependencies
 pdm add --no-sync -d mkdocs towncrier pytest tox dynaconf
 
-# init trunk, accept all defaults
-trunk init -y
-
 # init dynaconf
-pdm run dynaconf init -f toml --path .dynaconf/
+pdm run dynaconf init -f toml --path=.dynaconf/
 
 # init git repo and add to GitHub
-gh repo create
+gh repo create --private --source=. --remote=upstream
 
 # create .gitignore
 function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/\$@; }
 gi linux,macos,windows,python >.gitignore
+
+# init trunk, accept all defaults
+trunk init -y
